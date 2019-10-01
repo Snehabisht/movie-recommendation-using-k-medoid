8a2cb3f45c970a0d11b9200be739aa776c6124bb
Snehabisht <34576425+Snehabisht@users.noreply.github.com>
2019-10-01 15:33:24 +0530

    Add files via upload

---
+++ pearson.py
@@ -0,0 +1,9 @@
+ from scipy import stats
+'''np.array([0, 0, 0, 1, 1, 1, 1])
+b = np.arange(7)
+stats.pearsonr(a, b)
+(0.8660254037844386, 0.011724811003954649)
+'''
+ stats.pearsonr([1,5],[2,4.5])
+
+
